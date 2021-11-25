d24c3a23a72cd454b7ed3677c19924d2b75ef0bf
intelloger-mverma <manish.verma@intelloger.com>
2021-11-25 23:32:50 +0000

    updated

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ image = img_as_ubyte(gray)
 bins = np.array([0, 16, 32, 48, 64, 80, 96, 112, 128, 144, 160, 176, 192, 208, 224, 240, 255]) #16-bit
 inds = np.digitize(image, bins)
 
+# Main Calculation
 max_value = inds.max()+1
 matrix_coocurrence = greycomatrix(inds, [1], [0, np.pi/4, np.pi/2, 3*np.pi/4], levels=max_value, normed=False, symmetric=False)
 
